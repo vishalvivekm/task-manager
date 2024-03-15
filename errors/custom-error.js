49c76a75b6028ecfaba81265e64707fd15a8f96c
vishalvivekm <vishalvivek488@gmail.com>
2024-03-15 19:56:14 +0530

    add CustomAPIError class

---
+++ errors/custom-error.js
@@ -0,0 +1,18 @@
+
+// built-in Error class, extending from it
+
+class CustomAPIError extends Error {
+    constructor(message, statusCode) {
+    super(message)
+    this.statusCode = statusCode
+    }
+    }
+    
+    const createCustomError = (msg, statusCode) => {
+     return new CustomAPIError(msg, statusCode)
+    }
+    
+    module.exports =  {
+     createCustomError,
+     CustomAPIError,
+    }
