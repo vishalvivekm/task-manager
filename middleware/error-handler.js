4564a7a0646aac399a9bdeb4267676e6daef05d5
vishalvivekm <vishalvivek488@gmail.com>
2024-03-23 13:38:24 +0530

    add comments to error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const {CustomAPIError} = require('../errors/custom-error')
-const errorHandlerMiddleware = (err, req, res,next) => {
+const errorHandlerMiddleware = (err, req, res,next) => { // Error-handling middleware always takes four arguments.
     if ( err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({msg: err.message})
     }
@@ -7,3 +7,10 @@ const errorHandlerMiddleware = (err, req, res,next) => {
 }
 
 module.exports = errorHandlerMiddleware
+
+/*
+. You must provide four arguments to identify it as an error-handling middleware function. Even if you don’t need to use the next object, you must specify it to maintain the signature. Otherwise, the next object will be interpreted as regular middleware and will fail to handle errors.
+*/
+/*
+express docs on error-handling mws: https://expressjs.com/en/guide/using-middleware.html#middleware.error-handling
+*/
